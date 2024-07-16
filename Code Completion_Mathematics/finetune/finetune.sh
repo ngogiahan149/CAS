1e325884b722e3cfdcc4f918a2f954a56f66868a
Celia <ngogiahan20009@gmail.com>
2024-07-16 15:16:35 +0800

    upload code & mathematics tasks

---
+++ Code Completion_Mathematics/finetune/finetune.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DATA_PATH="evolinstruct_aug.json"
+OUTPUT_PATH="finetuned_model"
+MODEL_PATH="deepseek-ai/deepseek-coder-1.3b-instruct"
+
+deepspeed finetune_deepseekcoder.py \
+    --model_name_or_path $MODEL_PATH \
+    --data_path $DATA_PATH \
+    --output_dir $OUTPUT_PATH \
+    --num_train_epochs 3 \
+    --model_max_length 1024 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 4 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 100 \
+    --save_total_limit 100 \
+    --learning_rate 2e-5 \
+    --warmup_steps 10 \
+    --logging_steps 1 \
+    --lr_scheduler_type "cosine" \
+    --gradient_checkpointing True \
+    --report_to "tensorboard" \
+    --deepspeed configs/ds_config_zero3.json \
+    --bf16 True \
